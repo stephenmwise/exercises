e2e4248ba160b47045a5339bbc5609ad1962ca8f
stephenmwise <162519976+stephenmwise@users.noreply.github.com>
2024-08-18 19:58:09 -0400

    added content

--- 25-text_to_pdf/main.py
+++ 25-text_to_pdf/main.py
@@ -1,6 +1,7 @@
 import glob
 from fpdf import FPDF
 from pathlib import Path
+import pandas as pd
 
 # create a list of text filepaths
 filepaths = glob.glob("text_files/*txt")
@@ -19,6 +20,12 @@ for filepath in filepaths:
     # Add the name to the PDF
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=f"{newpdfname}", ln=1)
+
+    #Add content
+    with open(filepath, "r") as file:
+        content = file.read()
+    pdf.set_font(family="Times", size=12,)
+    pdf.multi_cell(w=0, h=6, txt=content)
 
 #Produce the PDF
 pdf.output("output.pdf")
